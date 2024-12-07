80122f5fb021e76e0ffb538a87d83ada67187026
Mark Hamann <mark@markhamann.com>
2024-12-06 17:59:00 -0800

    Get it working

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,18 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
-
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+void setup()
+{
+    Serial.begin(115200);
+    pinMode(PC13, OUTPUT);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void loop()
+{
+    static int loopCount = 0;
+    Serial.printf("Blink %d\n", loopCount);
+    digitalWrite(PC13, HIGH);
+    delay(100);
+    digitalWrite(PC13, LOW);
+    delay(100);
+    ++loopCount;
 }
